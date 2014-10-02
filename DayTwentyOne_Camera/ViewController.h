d571613f99743dc075184320b8e01694024da4ec
Anuj Katiyal <anujk3@gmail.com>
2014-10-03 00:41:06 +0530

    Initial Commit

---
+++ DayTwentyOne_Camera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DayTwentyOne_Camera
+//
+//  Created by Anuj Katiyal on 03/10/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
